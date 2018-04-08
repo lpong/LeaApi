9ae10296f44a4d91b1fb635f9781cbb599188ba0
lea <cotyxpp@gmail.com>
2018-04-08 13:47:54 +0800

    fixed import bug

--- application/validate/Project.php
+++ application/validate/Project.php
@@ -7,10 +7,11 @@ use think\Validate;
 class Project extends Validate
 {
     protected $rule = [
-        'name|项目名称'   => 'require|max:128',
-        'remark|项目介绍' => 'require|max:522',
-        'sort|排序'     => 'number',
-        'cover|封面'    => 'require',
+        'name|项目名称'        => 'require|max:128',
+        'remark|接口说明'      => 'require|max:522',
+        'description|项目描述' => 'require|max:522',
+        'sort|排序'          => 'number',
+        'cover|封面'         => 'require',
     ];
 
 }
