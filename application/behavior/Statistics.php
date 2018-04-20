bfec1d6c72bf9db66b3103f5a1fa498b7bf7dbb1
lea <cotyxpp@gmail.com>
2018-04-20 17:40:09 +0800

    fixed

---
+++ application/behavior/Statistics.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lea
+ * Date: 2018/4/9
+ * Time: 14:30
+ */
+
+namespace app\behavior;
+
+use think\Db;
+use think\Request;
+
+class Statistics
+{
+    public function run(Request $request)
+    {
+        return;
+    }
+}
